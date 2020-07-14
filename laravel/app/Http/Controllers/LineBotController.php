6f843e627c7ebe1ab74c5cdf0f1ca5390bc55be1
Sota <sooota09@gmail.com>
2020-07-14 16:04:46 +0900

    [add] コメント記述

--- laravel/app/Http/Controllers/LineBotController.php
+++ laravel/app/Http/Controllers/LineBotController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class LineBotController extends Controller
-{
+{   // LineBotContorollerにindexというメソッドを定義
     public function index()
     {
-        return view('linebot.index');
+        return view('linebot.index'); // linebotフォルダの中のindexという名前のviewファイルを表示
     }
 }
