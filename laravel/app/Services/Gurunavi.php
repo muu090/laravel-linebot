110f77121599fdc25ac8f4908434fc76f70279ee
Sota <sooota09@gmail.com>
2020-07-14 18:50:08 +0900

    [fix] '/parrot' =>'/restaurants' ルーティング修正

--- laravel/app/Services/Gurunavi.php
+++ laravel/app/Services/Gurunavi.php
@@ -6,15 +6,20 @@ use GuzzleHttp\Client;
 
 class Gurunavi
 {
-    public function searchRestaurants($word)
+    private const RESTAURANTS_SEARCH_API_URL = 'https://api.gnavi.co.jp/RestSearchAPI/v3/';
+
+    public function searchRestaurants(string $word): array
+     # ユーザから送られてくる$wordは文字列（string）であると宣言
+     # searchRestaurantsメソッドの戻り値が配列（array）であると宣言
     {
         $client = new Client();
         $response = $client
-            ->get('https://api.gnavi.co.jp/RestSearchAPI/v3/', [
+            ->get(self::RESTAURANTS_SEARCH_API_URL, [
                 'query' => [
                     'keyid' => env('GURUNAVI_ACCESS_KEY'),
                     'freeword' => str_replace(' ', ',', $word), # str_replace関数で半角スペースがあればカンマ（,）に変換
                 ],
+                'http_errors' => false,
             ]);
             
         return json_decode($response->getBody()->getContents(), true);
