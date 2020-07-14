4a7f5749b1361eda56f1f09e15ea191071eae465
Sota <sooota09@gmail.com>
2020-07-14 17:24:17 +0900

    [update] リクエストのログ出力設定

--- laravel/app/Http/Controllers/LineBotController.php
+++ laravel/app/Http/Controllers/LineBotController.php
@@ -4,10 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Log;
+
 class LineBotController extends Controller
 {   // LineBotContorollerにindexというメソッドを定義
     public function index()
     {
         return view('linebot.index'); // linebotフォルダの中のindexという名前のviewファイルを表示
+    }
+
+    public function parrot(Request $request)
+    {
+        Log::debug($request -> header());
+        Log::debug($request -> input());
     }
 }
