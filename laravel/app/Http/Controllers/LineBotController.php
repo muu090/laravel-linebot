2d93c3ef323037336d4853389c95ecd5854d48b6
Sota <sooota09@gmail.com>
2020-07-14 17:52:16 +0900

    [update] リクエストテキストをそのまま返す（オウム返し）

--- laravel/app/Http/Controllers/LineBotController.php
+++ laravel/app/Http/Controllers/LineBotController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Log;
+use LINE\LINEBot;
+use LINE\LINEBot\Event\MessageEvent\TextMessage;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
 
 class LineBotController extends Controller
 {   // LineBotContorollerにindexというメソッドを定義
@@ -15,7 +17,35 @@ class LineBotController extends Controller
 
     public function parrot(Request $request)
     {
+        // ログ出力設定
         Log::debug($request -> header());
         Log::debug($request -> input());
+
+        // LINEBotクラスを生成(インスタンス化)する
+        $httpClient = new CurlHTTPClient(env('LINE_ACCESS_TOKEN'));
+        $lineBot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANNEL_SECRET')]);
+
+        // 署名の検証を行う
+        $signature = $request -> header('x-line-signature');
+        if (!$lineBot ->validateSignature($request -> getContent(), $signature)) {
+            abort(400, 'Invalid signature');
+        }
+
+        // リクエストからイベントを取り出す
+        $events = $lineBot -> parseEventRequest($request -> getContent(), $signature);
+
+        Log::debug($events);
+
+        // LINEのチャンネルに返信する
+        foreach ($events as $event) {
+            if (!($event instanceof TextMessage)) {
+                Log::debug('Non text message has come');
+                continue;
+            }
+
+            $replyToken = $event -> getReplyToken();
+            $replyText = $event -> getText();
+            $lineBot -> replyText($replyToken, $replyText);
+        }
     }
 }
